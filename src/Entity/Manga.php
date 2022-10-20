5c48ec023356496a04569ea6884a8b9352a35834
lionel-kg <lionel77350@gmail.com>
2022-10-20 10:43:24 +0200

    create structure

---
+++ src/Entity/Manga.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MangaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MangaRepository::class)
+ */
+class Manga
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $originalName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOriginalName(): ?string
+    {
+        return $this->originalName;
+    }
+
+    public function setOriginalName(string $originalName): self
+    {
+        $this->originalName = $originalName;
+
+        return $this;
+    }
+}
