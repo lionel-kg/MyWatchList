6719a4cf1c272c47689de93740aaf12bfad3e448
lionel-kg <lionel77350@gmail.com>
2022-10-20 10:45:09 +0200

    create structure

---
+++ src/Controller/MangaController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MangaController extends AbstractController
+{
+    /**
+     * @Route("/manga", name="app_manga")
+     */
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MangaController.php',
+        ]);
+    }
+}
